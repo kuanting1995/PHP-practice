1012cce6d53f66c09b078a6442555cacf01f109b
kuanting1995 <116135898+kuanting1995@users.noreply.github.com>
2023-05-22 16:03:03 +0800

    update

---
+++ echo.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+    <?php
+    //echo
+    echo "<h2>Hello php</h2>"; //php內部可使用html標記 
+    $msg = "MyFirstPHP"; //變數使用$宣告
+    echo "<h1>Message is = $msg</h1>";
+    echo "Hello \"world\"!"; //跳脫字元
+    echo "Hello by PHP print  
+this is multi line  
+text printed by   
+PHP print statement";
+    //php echo 和 print 換行使用html語法 ex:<br>
+    ?>
+</body>
+
+</html>
---
+++ print.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+    <?php
+    //print-有返回值，可視為函式，返回值總是為1
+    //print用途-列印字串,變數,陣列等 int print(string $arg)
+    //print1:
+    print "Hello php Print";
+    print "Hello by PHP print  
+this is multi line  
+text printed by   
+PHP print statement '\n' <br>";
+    $msg = "Hello print() in PHP";
+    print " \"Message\" is: $msg";
+    ?>
+</body>
+
+</html>
